40c6552f994ec9913a1d9144f7679151a8037993
Pollakthegoat69 <m24161@hb.dhbw-stuttgart.de>
2025-05-12 12:52:35 +0200

    Aufgabe 1

--- 04_Spiele/Spiel_01.py
+++ 04_Spiele/Spiel_01.py
@@ -1,1 +1,9 @@
-print("halloWelt")
+kartenStapel = ["gr7", "gl7", "+4"]
+while len(kartenStapel) != 0:
+    print(kartenStapel)
+    aktuelleKarte = kartenStapel.pop(0)
+
+    if aktuelle Karte == "+4" :
+    print ("+4 kann gespielt werden")
+    break 
+print("Stapel ist leer")
