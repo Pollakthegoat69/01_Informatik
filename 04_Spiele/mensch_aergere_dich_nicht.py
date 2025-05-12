ebf48113b279d9c16625da28e6fbd6d21a0e319b
Pollakthegoat69 <m24161@hb.dhbw-stuttgart.de>
2025-05-12 14:23:54 +0000

    aufgabe

---
+++ 04_Spiele/mensch_aergere_dich_nicht.py
@@ -0,0 +1,39 @@
+import random
+würfelzahl =random.randint(1,6)
+print("du hast die Zahl", würfelzahl ,"geworfen")
+abfrage = input("Sind alle Figuren im Haus? (ja/nein?)")
+Anzahlwurf = 0
+if abfrage = "nein"
+   
+    if würfelzahl == 6
+        antwort = input ("ist mindestens eine Figur im Haus? (ja/nein)")
+       
+        if antwort == "ja":
+        antwort2 = input("ist das Startfeld frei?(ja/nein)"")
+        
+            if antwort2 == "ja":
+                print("bringe die Figur is Spiel")
+            
+            else:
+                print("du musst die Figur auf dem Feld bewegen")
+        
+        if antwort == "nein":
+            print("laufe mit der Figur um die Augenzahl nach vorne")
+            
+    else:
+        print("bewege die Figur um die Zahl nach vorne")
+        break 
+        
+if abfrage = "ja":
+    
+    if würfelzahl == 6:
+            print("Du darfst deine figur zum Start bewegen")
+
+    else:
+        if AnzahlWurf == 3
+        break 
+       
+        else:
+            print("neuer Versuch")
+
+    AnzahlWurf = AnzahlWurf + 1
